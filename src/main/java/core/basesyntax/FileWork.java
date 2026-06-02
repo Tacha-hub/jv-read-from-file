3cdacaff930e55643e3dcccc04ea454105f8c82c
Anastasiia Volichenko <anastasiia.volichenko@gmail.com>
2026-06-02 14:30:29 +0100

    the first solution at 14:30

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,28 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            ArrayList<String> list = new ArrayList<>();
+            Scanner scanner = new Scanner(new File(fileName));
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                word = word.replaceAll("[^a-zA-Z]", "");
+                if (!word.isEmpty() && (word.startsWith("w") || word.startsWith("W"))) {
+                    list.add(word.toLowerCase());
+                }
+            }
+            scanner.close();
+            Collections.sort(list);
+            return list.toArray(new String[0]);
+        } catch (IOException e) {
+            throw new RuntimeException("File not found: ", e);
+        }
     }
 }
